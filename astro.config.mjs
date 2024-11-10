b8c1f1c26221056e735d3e6eb5a7914952bf6b72
ryoK <test@example.com>
2024-11-10 15:40:09 +0900

    chore: GitHub Pagesデプロイメントのワークフローを追加し、サイトのURLとベースパスを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://BIGtanukiudon.github.io",
+  base: "/activity-pulse-logger-page",
 });
